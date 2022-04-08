0c1ca8e49297abecda5b73154196e302521f6bf0
changliao1025 <changliao1025@outlook.com>
2022-04-08 12:38:11 -0700

    rm rst

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DESCRIPTION = \
 AUTHOR = "Chang Liao"
 AUTHOR_EMAIL = "chang.liao@pnnl.gov"
 URL = "https://github.com/changliao1025/pyhexwatershed"
-VERSION = "0.1.4"
+VERSION = "0.1.5"
 REQUIRES_PYTHON = ">=3.8.0"
 KEYWORDS = "hexwatershed hexagon"
 
