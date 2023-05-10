918159411ff6f68ca41317bb47ab4042ae384bf1
changliao1025 <changliao1025@outlook.com>
2023-05-10 12:37:58 -0700

    fix geology

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ DESCRIPTION = \
 AUTHOR = "Chang Liao"
 AUTHOR_EMAIL = "chang.liao@pnnl.gov"
 URL = "https://github.com/changliao1025/pyhexwatershed"
-VERSION = "0.2.13"
+VERSION = "0.2.15"
 REQUIRES_PYTHON = ">=3.8.0"
 KEYWORDS = "hexwatershed hexagon"
 
